225a42e69cbeaa4838c818bbeb16e29942ee884f
Girish2401 <50285125+Girish2401@users.noreply.github.com>
2019-05-17 15:08:46 +0530

    finding lenth of linklist

---
+++ length of a linklist.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+int data;
+struct node* next;
+};
+void insert(struct node **headref,int data)
+{
+struct node *temp=(struct node*)malloc(sizeof(struct node));
+temp->data=data;
+temp->next=*headref;
+*headref=temp;
+}
+
+void print(struct node **headref)
+{
+struct node* temp=*headref;
+while(temp!=NULL)
+{
+printf("%d->",temp->data);
+temp=temp->next;
+}
+}
+int length(struct node** headref,int count)
+{
+    struct node* temp=*headref;
+    while(temp!=NULL)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count;
+}
+int main()
+{
+    int count;
+struct node* head=NULL;
+insert(&head,5);
+insert(&head,7);
+insert(&head,10);
+print(&head);
+ count=length(&head,0);
+cout<<"length of the list is"<<count;
+}
